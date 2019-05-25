b0106ec1e1d8aa4fbae50dd5c2d24dd1dc151650
Jose Ernesto Calvimontes <joecalvimontes@hotmail.com>
2019-05-25 13:29:00 -0400

    commit con las clases creadas de todas las carpetas

---
+++ Parcial2JC/Parcial2JC/Parcial2JC/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Parcial2JC.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+
+    internal class DisplayAttribute : Attribute
+    {
+        public string Name;
+    }
+
+    internal class RequiredAttribute : Attribute
+    {
+    }
+
+    internal class KeyAttribute : Attribute
+    {
+    }
+}
---
+++ Parcial2JC/Parcial2JC/Parcial2JC/ViewModels/LoginViewModel.cs
@@ -0,0 +1,154 @@
+﻿namespace Parcial2JC.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Windows.Input;
+    using Services;
+    using Xamarin.Forms;
+    using Models;
+    using Views;
+    using Parcial2Jc.ViewModels;
+
+    public class LoginViewModel : BaseViewModel
+    {
+        #region Attributes
+        string email;
+        string password;
+        bool isrunning;
+        bool isenabled;
+        ApiService apiService;
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                SetValue(ref this.email, value);
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                SetValue(ref this.password, value);
+            }
+        }
+        public bool IsRunning
+        {
+            get
+            {
+                return this.isrunning;
+            }
+            set
+            {
+                SetValue(ref this.isrunning, value);
+            }
+
+        }
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isenabled;
+            }
+            set
+            {
+                SetValue(ref this.isenabled, value);
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(cmdLogin);
+            }
+        }
+
+        private async void cmdLogin()
+        {
+            if (String.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Email empty",
+                                "Please enter your email",
+                                "Accept");
+                return;
+            }
+            if (String.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Password empty",
+                                "Please enter your password",
+                                "Accept");
+                return;
+            }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            var conexion = await this.apiService.CheckConnection();
+            if (!conexion.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                   "ERROR",
+                   conexion.Message,
+                   "Accept");
+                return;
+            }
+
+            TokenResponse token = await this.apiService.GetToken(
+                   "https://productosi220.azurewebsites.net",
+                   this.Email,
+                   this.Password);
+            if (token == null)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                   "ERROR",
+                   "Something was wrong, please try later.",
+                   "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(token.AccessToken))
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                   "ERROR",
+                   token.ErrorDescription,
+                   "Accept");
+                this.Password = String.Empty;
+
+                return;
+            }
+
+            MainViewModel mainViewModel = MainViewModel.GetInstance();
+            mainViewModel.Token = token.AccessToken;
+            mainViewModel.TokenType = token.TokenType;
+
+            Application.Current.MainPage = new NavigationPage(new ProductPage());
+            IsRunning = false;
+            IsEnabled = true;
+        }
+        #endregion
+
+        public LoginViewModel()
+        {
+
+        }
+    }
+}
